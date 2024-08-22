287302ef859c50c3c64d412b61517b1a2e98a2e1
gokhangnccn <gokhangnccn@gmail.com>
2024-08-22 21:31:47 +0300

    IndextekiniCikar eklendi

---
+++ patika-csharp-kurs/IndextekiniCikar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_csharp_kurs
+{
+    public class IndextekiniCikar
+    {
+        string str = "Algoritma";
+        string bos = "";
+        public string indextekiniCikar(int index)
+        {
+            for(int i=0; i<str.Length; i++)
+            {
+                if (i != index)
+                {
+                    bos += str[i];
+                }
+            }
+            return bos;
+        }
+    }
+}
--- patika-csharp-kurs/Program.cs
+++ patika-csharp-kurs/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using patika_csharp_kurs;
 using PatikaExamples.FibonacciTriangle;
 using static patikaDevOdevleri.UcgenCiz;
 
@@ -12,11 +13,16 @@ namespace patikaDevOdevleri
     {
         public static void Main(string[] args)
         {
+
             int height = 0;
             Console.WriteLine("Kullanacağınız Uygulamanın Sayısını Giriniz:");
             Console.WriteLine("1. Fibonacci serisi hesaplama");
             Console.WriteLine("2. Konsola Üçgen Çizme:");
             Console.WriteLine("3. Yarıçapa göre konsola daire çizdir:");
+            Console.WriteLine("4. Stringteki seçili indexteki harfi sil:");
+
+            Console.WriteLine("0. Çık:");
+
             Console.WriteLine("Kullanacağınız Uygulamayı Giriniz:");
             int secenek = int.Parse(Console.ReadLine());
 
@@ -41,15 +47,14 @@ namespace patikaDevOdevleri
                     dciz.DrawCircle(height);
                     break;
                 case 4:
+                    Console.WriteLine("Silinecek indexi seçiniz:");
+                    IndextekiniCikar cikar = new IndextekiniCikar();
+                    Console.WriteLine(cikar.indextekiniCikar(GetInputIndex()));
                     break;
+                
             }
 
-
-            
-
-            
         }
-
         private static int GetInputFromUser()
         {
             Console.Write("Lütfen değer giriniz: ");
@@ -61,6 +66,19 @@ namespace patikaDevOdevleri
             {
                 Console.WriteLine("Geçersiz bir giriş yapıldı. Lütfen pozitif bir tam sayı giriniz.");
                 return GetInputFromUser(); 
+            }
+        }
+        private static int GetInputIndex()
+        {
+            Console.Write("Lütfen değer giriniz: ");
+            if (int.TryParse(Console.ReadLine(), out int height) && height >= 0)
+            {
+                return height;
+            }
+            else
+            {
+                Console.WriteLine("Geçersiz bir giriş yapıldı. Lütfen pozitif bir tam sayı giriniz.");
+                return GetInputIndex();
             }
         }
     }
